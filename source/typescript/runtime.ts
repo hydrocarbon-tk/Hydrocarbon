a4db23b376cd4f15356f9937a6198bdab2fd3782
Anthony Weathersby <anthonycweathersby@gmail.com>
2020-07-05 13:03:32 -0600

    Add runtime module

---
+++ source/typescript/runtime.ts
@@ -0,0 +1,15 @@
+
+//Runtime
+import { lrParse } from "./lr/runtime/lr_parser.js";
+import { ParserEnvironment } from "./types/parser_environment.js";
+import { ErrorHandler, ParserData } from "./types/parser_data.js";
+
+
+//Definitions
+
+export {
+    ErrorHandler,
+    ParserData,
+    ParserEnvironment,
+    lrParse
+};
