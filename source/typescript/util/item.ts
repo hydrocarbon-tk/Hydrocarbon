42f38a7fe99df838bbbe43a833bfad0664fcb254
Anthony Weathersby <anthonycweathersby@gmail.com>
2020-08-28 17:37:26 -0600

    Modify item.ts

--- source/typescript/util/item.ts
+++ source/typescript/util/item.ts
@@ -1,6 +1,6 @@
-import { ProductionBody, Grammar, Symbol, SymbolType } from "../types/grammar.js";
+import { ProductionBody, Grammar, Symbol, SymbolType, Production } from "../types/grammar.js";
 
-export function SymbolToString(sym:{type?:SymbolType, val:string}) {
+export function SymbolToString(sym: { type?: SymbolType, val: string; }) {
     switch (sym.type) {
         case SymbolType.ESCAPED:
         case SymbolType.SYMBOL:
@@ -18,32 +18,51 @@ export function SymbolToString(sym:{type?:SymbolType, val:string}) {
     }
 }
 
+export function SymbolToStringUnformatted(sym: { type?: SymbolType, val: string; }) {
+    switch (sym.type) {
+        case SymbolType.ESCAPED:
+        case SymbolType.SYMBOL:
+            return `${sym.val}`;
+        case SymbolType.GENERATED:
+            return `θ${sym.val}`;
+        case SymbolType.LITERAL:
+            return `${sym.val}`;
+        case SymbolType.EMPTY:
+            return `ɛ`;
+        case SymbolType.END_OF_FILE:
+            return `$eof`;
+        default:
+            return sym.val;
+    }
+}
+
 export class Item extends Array {
 
-    static fromArray(array: Item) : Item {
+    static fromArray(array: Item): Item {
+        if (array instanceof Item) return array;
         return new Item(array[0], array[1], array[2], array.follow);
     }
 
-    USED:boolean;
-    follow:Symbol;
+    USED: boolean;
+    follow: Symbol;
 
-    constructor(body_id:number, length:number, offset:number, follow:Symbol) {
+    constructor(body_id: number, length: number, offset: number, follow: Symbol) {
         super(...[body_id, length, offset]);
-        
+
         this.follow = follow;
-        
+
         this.USED = false;
     }
 
-    get atEND() : boolean {
+    get atEND(): boolean {
         return this.offset >= this.len;
     }
 
-    get v() : string | number {
+    get v(): string | number {
         return this.follow.val;
     }
 
-    get p() :number {
+    get p(): number {
         return this.follow.precedence;
     }
 
@@ -51,15 +70,15 @@ export class Item extends Array {
         return "" + this.body + "" + this.len + "" + this.offset + "|";
     }
 
-    get full_id() : string{
+    get full_id(): string {
         return this.id + this.v;
     }
 
-    get body() : number{
+    get body(): number {
         return this[0];
     }
 
-    get len() : number{
+    get len(): number {
         return this[1];
     }
 
@@ -68,18 +87,18 @@ export class Item extends Array {
         return this[2];
     }
 
-    body_(grammar:Grammar):ProductionBody {
+    body_(grammar: Grammar): ProductionBody {
         return grammar.bodies[this.body];
     }
 
-    sym(grammar:Grammar): Symbol {
+    sym(grammar: Grammar): Symbol {
         return this.body_(grammar).sym[this.offset];
     }
 
-    render(grammar:Grammar) :string {
-        
+    render(grammar: Grammar): string {
+
         const a = this.body_(grammar).sym
-            .map(sym => sym.type == "production" ? { val: "\x1b[38;5;8m" + grammar[sym.val].name.replace(/\$/, "::\x1b[38;5;153m") } : sym)
+            .map(sym => sym.type == SymbolType.PRODUCTION ? { val: "\x1b[38;5;8m" + grammar[sym.val].name.replace(/\$/, "::\x1b[38;5;153m") } : sym)
             //@ts-ignore
             .flatMap((sym, i) => (i == this.offset) ? ["\x1b[38;5;226m•", SymbolToString(sym)] : SymbolToString(sym));
         if (a.length == this.offset)
@@ -87,38 +106,62 @@ export class Item extends Array {
         return a.join(" ");
     }
 
-    renderWithProduction(grammar:Grammar):string {
+    renderUnformatted(grammar: Grammar): string {
+
+        const a = this.body_(grammar).sym
+            .map(sym => sym.type == SymbolType.PRODUCTION ? { val: grammar[sym.val].name } : sym)
+            //@ts-ignore
+            .flatMap((sym, i) => (i == this.offset) ? [".", SymbolToStringUnformatted(sym)] : SymbolToStringUnformatted(sym));
+        if (a.length == this.offset)
+            a.push("•");
+        return a.join(" ");
+    }
+
+    renderWithProduction(grammar: Grammar): string {
         return `\x1b[48;5;233m\x1b[38;5;226m[ ${this.renderProductionName(grammar)} \x1b[38;5;226m⇒ ${this.render(grammar)} \x1b[38;5;226m]\x1b[0m`;
     }
 
-    renderWithProductionAndFollow(grammar:Grammar):string {
+    renderWithProductionAndFollow(grammar: Grammar): string {
         //@ts-ignore
-        return `[ ${this.body_(grammar).production.name.replace(/\$/, "::")}\x1b[0m ⇒ ${this.render(grammar)}, ${this.v}]`;
+        return `\x1b[48;5;233m\x1b[38;5;226m[ ${this.renderProductionName(grammar)} \x1b[38;5;226m⇒ ${this.render(grammar)}, ${this.v} \x1b[38;5;226m]\x1b[0m`;;
     }
 
-    renderProductionName(grammar:Grammar):string{
+    getProduction(grammar: Grammar): Production {
+        //@ts-ignore
+        return this.body_(grammar).production;
+    }
+
+    getProductionAtSymbol(grammar: Grammar): Production {
+        //@ts-ignore
+        const prod = this.sym(grammar).val;
+
+        return grammar[prod];
+    }
+
+
+    renderProductionName(grammar: Grammar): string {
         //@ts-ignore
         return `\x1b[38;5;8m${this.body_(grammar).production.name.replace(/\$/, "::\x1b[38;5;153m")}`;
     }
 
-    renderProductionNameWithBackground(grammar):string{
+    renderProductionNameWithBackground(grammar): string {
         //@ts-ignore
         return `\x1b[48;5;233m\x1b[38;5;8m ${this.body_(grammar).production.name.replace(/\$/, "::\x1b[38;5;153m")} \x1b[0m`;
     }
 
-    renderSymbol(grammar:Grammar):string{
+    renderSymbol(grammar: Grammar): string {
         const sym = this.sym(grammar);
         //@ts-ignore
         return `\x1b[48;5;233m ${SymbolToString(sym)} \x1b[0m`;
     }
 
-    increment() : Item | null {
+    increment(): Item | null {
         if (this.offset < this.len)
             return new Item(this.body, this.len, this.offset + 1, this.follow);
         return null;
     }
 
-    match(item:Item) :boolean {
+    match(item: Item): boolean {
         return item.id == this.id;
     }
 
