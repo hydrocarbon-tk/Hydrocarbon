6f900736c063c6db288c2b1f22c15ff4b4018cbc
Anthony Weathersby <acweathersby.codes@gmail.com>
2021-06-12 17:08:37 -0600

    Chore: Update test

--- test/languages/mock_json.spec.js
+++ test/languages/mock_json.spec.js
@@ -2,21 +2,19 @@ import URL from "@candlelib/uri";
 import { compileGrammarSource } from "../tools.js";
 
 await URL.server();
-assert_group(sequence, 10000, () => {
 
-    const parse_test_1_result = { array: [1, 2, { taco: 'pub' }], number: 25.25 };
+const parse_test_1_result = { array: [1, 2, { taco: 'pub' }], number: 25.25 };
 
-    //Construct A HCG parser
-    const mock_js_source = await(URL.resolveRelative("./test/languages/mock.json.hcg").fetchText());
+//Construct A HCG parser
+const mock_js_source = await (URL.resolveRelative("./test/languages/mock.json.hcg").fetchText());
 
-    const mock_js_parser = await compileGrammarSource(mock_js_source);
+const mock_js_parser = await compileGrammarSource(mock_js_source);
 
-    const data_string =
-        `{ 
+const data_string =
+    `{ 
         "array":[1,2, { "taco" : "pub" }],
         "number": 25.25e0
     }`;
 
-    assert(i, "Parsed Input Produces Expected Value", mock_js_parser(data_string).result[0] == parse_test_1_result);
+assert("Parsed Input Produces Expected Value", mock_js_parser(data_string).result[0] == parse_test_1_result);
 
-});
