9513108a78ae3efea85129d304e0948649c4485b
Anthony Weathersby <acweathersby.codes@gmail.com>
2021-06-15 14:20:58 -0600

    Misc: Modify CLI: Add create-staged sub command

--- source/typescript/entry/cli.ts
+++ source/typescript/entry/cli.ts
@@ -7,9 +7,12 @@
 import {
     addCLIConfig, processCLIConfig
 } from "@candlelib/paraffin";
+import URI from "@candlelib/uri";
 import URL from "@candlelib/uri";
-import { compileRecognizer, writeParserScriptFile } from "../build/compiler.js";
+import { buildRecognizer } from "../build/build.js";
 import { compileGrammarFromURI } from "../grammar/compile.js";
+import { createAddHocParser } from "../render/create_add_hoc_parser.js";
+import { generateWebAssemblyParser, writeParserScriptFile } from "../render/render.js";
 
 await URL.server();
 
@@ -42,7 +45,10 @@ const number_of_workers = addCLIConfig("compile", {
 addCLIConfig("compile", {
     key: "compile",
     help_brief: `
-    Usage: compile <path_to_source_grammar>`
+    Usage: compile <path_to_source_grammar>
+    
+    Compile new parser from an hydrocarbon grammar source file (.hcg)
+`
 }).callback = (async (args) => {
 
     const path = args.trailing_arguments.pop();
@@ -60,13 +66,40 @@ addCLIConfig("compile", {
 
     const grammar = await compileGrammarFromURI(file_path);
 
-    const { meta, recognizer_functions } = await compileRecognizer(grammar, threads);
+    const { meta, recognizer_functions } = await buildRecognizer(grammar, threads);
 
     switch (type.value) {
         default:
             await writeParserScriptFile(output + "", grammar, recognizer_functions, meta);
 
     }
+});
+
+
+addCLIConfig("create-staged", {
+    key: "create-staged",
+    help_brief: `
+    Usage: create-staged
+    
+    Create a new HCG parser and place in @candlelib/hydrocarbon/build/staged folder
+`
+}).callback = (async (args) => {
+    const
+        hcg_grammar_file = URI.resolveRelative("./source/grammars/hcg-3-alpha/hcg.hcg");
+
+    const compiled_grammar = await compileGrammarFromURI(hcg_grammar_file);
+    const bootstrapped_compiled_grammar = await compileGrammarFromURI(hcg_grammar_file);
+    const {
+        recognizer_functions: bootstrapped_recognizer_functions,
+        meta: bootstrapped_meta,
+    } = await buildRecognizer(bootstrapped_compiled_grammar, 1);
+
+    let SUCCESS = await writeParserScriptFile(
+        URI.resolveRelative("./build/staged/hcg3_parser.staged.ts") + "",
+        bootstrapped_compiled_grammar, bootstrapped_recognizer_functions, bootstrapped_meta,
+        "../runtime/parser_loader_next.js",
+        generateWebAssemblyParser
+    );
 });
 
 processCLIConfig();
