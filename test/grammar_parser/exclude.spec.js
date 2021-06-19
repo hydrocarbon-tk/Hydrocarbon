62bc9963a5c4c55a07793618adacdb17f7c11d57
Anthony Weathersby <acweathersby.codes@gmail.com>
2021-06-19 11:47:11 -0600

    Chore: Update tests

---
+++ test/grammar_parser/exclude.spec.js
@@ -0,0 +1,29 @@
+import { compileGrammar } from "../../build/library/grammar/compile.js";
+import { render } from "../../build/library/grammar/passes/common.js";
+import { loadGrammarFromString } from "../../build/library/grammar/load.js";
+import { compileJSParserFromGrammar } from "../tools.js";
+assert_group(sequence, 10000, () => {
+
+    const test_grammar_string =
+        `@IGNORE g:ws g:nl
+
+        <> start > A
+
+        <> A > \\EXC (EXC \\fn \\[ ) B? \\tx f:r {  $1 + $2 + $3  }
+            | \\td C \\ty
+                  
+        <> B > \\tx C \\ty
+             | C
+
+        <> C > \\tx \\ty  | \\fn \\[  \\test \\]
+        `;
+
+    const test_grammar = await loadGrammarFromString(test_grammar_string);
+    ;
+    const grammar = await compileGrammar(test_grammar);
+
+    console.log(test_grammar.productions[1], render(test_grammar.productions[1]));
+
+    assert(i, grammar.productions[1].bodies.length == 3);
+
+});
---
+++ test/grammar_parser/production_collapsing.spec.js
@@ -0,0 +1,32 @@
+import { compileGrammar } from "../../build/library/grammar/compile.js";
+import { loadGrammarFromString } from "../../build/library/grammar/load.js";
+
+const test_grammar_string =
+    `@IGNORE g:ws g:nl
+
+        <> start > A f:r { $1 + "test" }
+        
+        <> A >  B | C
+                  
+        <> B > A \\tz  |  \\tx C \\ty C
+             | C \\tz
+             | D
+
+        <> D > E
+
+        <> E > R f:r { $1 + "test" }
+
+        <> R > G
+
+        <> G >G \\tz
+            | \\tr
+            | \( D \)
+
+        <> C >  \\tx \\ty C
+            | \\ty \\tx 
+        `;
+
+const test_grammar = await loadGrammarFromString(test_grammar_string);
+const grammar = await compileGrammar(test_grammar);
+
+assert("Should merge body of productions into referencing bodies if there is no reduce action or conflicting recursion", grammar.productions[1].bodies.length == 5);
