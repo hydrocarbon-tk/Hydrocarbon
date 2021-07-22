ab744be70175b79501b93c0ee86d2a0a205d8f4a
Anthony Weathersby <acweathersby.codes@gmail.com>
2021-07-22 10:14:41 -0600

    Chore: Modify runtime module exports

--- source/typescript/entry/runtime.ts
+++ source/typescript/entry/runtime.ts
@@ -4,17 +4,35 @@
  * disclaimer notice.
  */
 
-//Runtime
-import { ParserEnvironment } from "../types/parser_environment.js";
-import { ParserFactory } from "../runtime/parser_loader.js";
+import "../utilities/array_globals.js";
+
+
+//Runtime - Hybrid
+import { initializeUTFLookupTable } from "../runtime/parser_memory_new.js";
 import { loadWASM, buildParserMemoryBuffer } from "../runtime/parser_memory_old.js";
+import { ParserFactory } from "../runtime/parser_loader.js";
+import { ParserFactory as ParserFactoryNew } from "../runtime/parser_loader_alpha.js";
+import { ParserFactory as ParserFactoryNext } from "../runtime/parser_loader_next.js";
+import { Token } from "../runtime/token.js";
 
+//Diagnostics
+import { ParserEnvironment } from "../types/parser_environment.js";
+import { HCGParser } from "../types/parser.js";
 
-//Definitions
 
 export {
-    ParserFactory,
+    //Types
+    HCGParser,
     ParserEnvironment,
+    Token,
+
+    //Code
     loadWASM,
-    buildParserMemoryBuffer
+    ParserFactory,
+    ParserFactoryNew,
+    ParserFactoryNext,
+    buildParserMemoryBuffer,
+
+    initializeUTFLookupTable,
 };
+
