9a5a241dcd9feff18caf9d9eae45f2b00a068977
Anthony Weathersby <acweathersby.codes@gmail.com>
2021-09-06 22:10:18 -0600

    Chore: Add magic number variable

---
+++ source/typescript/build/ir_reduce_numeric_len_id.ts
@@ -0,0 +1,7 @@
+/**
+ * A "magic" number assigned to a reduce node's length
+ * value to indicate that it is to use the symbol accumulator
+ * to determine how many symbols are to removed from the
+ * parse stack when the reduce function is called.
+ */
+export const ir_reduce_numeric_len_id = 0x90FA0102;
