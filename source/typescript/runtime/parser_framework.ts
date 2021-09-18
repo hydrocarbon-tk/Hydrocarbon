7a2ad5d5baff8da136ed28d554ec21b46e1439d9
Anthony Weathersby <acweathersby.codes@gmail.com>
2021-09-18 17:56:28 -0600

    Feat: Catch all errors in during completing and pass into return object

--- source/typescript/runtime/parser_framework.ts
+++ source/typescript/runtime/parser_framework.ts
@@ -63,6 +63,7 @@ export async function ParserFramework<T, R, K extends keyof R>(
 
         if (valid.have_valid()) {
 
+
             const
                 data = valid.get_mut_state(0),
                 history = data.state_history.slice(),
@@ -74,73 +75,77 @@ export async function ParserFramework<T, R, K extends keyof R>(
                 rules = [],
                 token_offset = 0;
 
+            try {
 
-            while (iter.is_valid()) {
+                while (iter.is_valid()) {
 
-                let low = iter.next();
+                    let low = iter.next();
 
-                rules.push(low);
+                    rules.push(low);
 
-                if (low == 0) break;
+                    if (low == 0) break;
 
-                const rule = low & 3;
+                    const rule = low & 3;
 
-                switch (rule) {
-                    case 0: //REDUCE;
-                        {
-                            let
-                                body = (low >> 8) & 0xFF,
-                                len = ((low >> 3) & 0x1F);
+                    switch (rule) {
+                        case 0: //REDUCE;
+                            {
+                                let
+                                    body = (low >> 8) & 0xFF,
+                                    len = ((low >> 3) & 0x1F);
 
-                            if (low & 4) {
-                                body = (low >> 3);
-                                len = iter.next();
-                            }
+                                if (low & 4) {
+                                    body = (low >> 3);
+                                    len = iter.next();
+                                }
 
-                            const
-                                pos_a = tokens[tokens.length - len] || default_token,
+                                const
+                                    pos_a = tokens[tokens.length - len] || default_token,
 
-                                pos_b = tokens[tokens.length - 1] || default_token,
+                                    pos_b = tokens[tokens.length - 1] || default_token,
 
-                                e = stack.slice(-len),
+                                    e = stack.slice(-len),
 
-                                token = Token.fromRange(pos_a, pos_b);
+                                    token = Token.fromRange(pos_a, pos_b);
 
-                            tokens[stack.length - len] = token;
+                                tokens[stack.length - len] = token;
 
-                            stack[stack.length - len] = fns[body](env, e, token);
+                                stack[stack.length - len] = fns[body](env, e, token);
 
-                            stack.length = stack.length - len + 1;
+                                stack.length = stack.length - len + 1;
 
-                            tokens.length = tokens.length - len + 1;
+                                tokens.length = tokens.length - len + 1;
+
+                            } break;
+
+                        case 1: { //SHIFT;
+
+                            let length = (low >>> 3) & 0x1FFF;
+
+                            if (low & 4) length = ((length << 16) | iter.next());
+
+                            stack.push(input_string.slice(token_offset, token_offset + length));
+
+                            tokens.push(new Token(input_string, "", length, token_offset));
+
+                            token_offset += length;
 
                         } break;
 
-                    case 1: { //SHIFT;
+                        case 2: { //SKIP
 
-                        let length = (low >>> 3) & 0x1FFF;
+                            let length = (low >>> 3) & 0x1FFF;
 
-                        if (low & 4) length = ((length << 16) | iter.next());
+                            if (low & 4) length = ((length << 16) | iter.next());
 
-                        stack.push(input_string.slice(token_offset, token_offset + length));
-
-                        tokens.push(new Token(input_string, "", length, token_offset));
-
-                        token_offset += length;
-
-                    } break;
-
-                    case 2: { //SKIP
-
-                        let length = (low >>> 3) & 0x1FFF;
-
-                        if (low & 4) length = ((length << 16) | iter.next());
-
-                        token_offset += length;
+                            token_offset += length;
+                        }
                     }
                 }
+                return { result: stack, history: history, err: null };
+            } catch (e) {
+                return { result: [], history: history, err: e };
             }
-            return { result: stack, history: history };
         }
 
         if (invalid.len() > 0) {
