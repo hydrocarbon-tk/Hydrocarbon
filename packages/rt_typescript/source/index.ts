fe95891060154c0fb8ab3c3e087f8d414e38262c
Anthony Weathersby <acweathersby.codes@gmail.com>
2021-11-15 21:42:20 -0700

    WIP: Re-export runtime types from common

--- packages/rt_typescript/source/index.ts
+++ packages/rt_typescript/source/index.ts
@@ -1,2 +1,24 @@
+export {
+    ByteReader,
+    ByteWriter,
+    F32Type,
+    F64Type,
+    I16Type,
+    I32Type,
+    I64Type,
+    I8Type,
+    StringType,
+    StructVectorType,
+    Token,
+    TokenType,
+    VectorType
+} from '@hctoolkit/common';
 export * from "./completer/complete";
 export * from "./types/ast_node";
+
+
+
+
+
+
+
