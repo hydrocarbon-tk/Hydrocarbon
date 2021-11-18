ac26023be2b9a3173630cdbed10cb4b2be9596fc
Anthony Weathersby <acweathersby.codes@gmail.com>
2021-11-18 14:40:03 -0700

    Add script to collect publish bounties

---
+++ scripts/collect_publish_bounties.js
@@ -0,0 +1,36 @@
+#! /usr/bin/node
+
+import {
+    createDepend
+} from "@candlelib/dev-tools";
+import { getPackageJsonObject } from "@candlelib/paraffin";
+import URI from "@candlelib/uri";
+import { exec } from "child_process";
+await URI.server();
+
+// Grab the names of the hctoolkit packages of which we
+// will version.
+const { package: wksp_pkg, FOUND } = await getPackageJsonObject(URI.getCWDURL());
+
+if (FOUND && wksp_pkg.name == "@hctoolkit/workspace") {
+
+    const target_packages = await Promise.all(wksp_pkg.devPackages.map(createDepend));
+
+
+    for (const target of target_packages) {
+        const bounty_path = URI.resolveRelative("./publish.bounty", target.package._workspace_location + "/");
+        if (await bounty_path.DOES_THIS_EXIST()) {
+            console.log(bounty_path);
+            exec(bounty_path + "", {
+                cwd: bounty_path.dir
+            }, (error, stdout) => {
+                console.log(stdout);
+                if (error)
+                    console.log(error); // an AbortError
+            });
+        }
+    }
+
+} else {
+    throw new Error("Unable to locate workspace package.json");
+}
