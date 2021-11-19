42bd55d4ab3915d9081eb6a95b2cbf50488ee2ec
Anthony Weathersby <acweathersby.codes@gmail.com>
2021-11-19 13:55:08 -0700

    Add deploy steps

--- scripts/version.js
+++ scripts/version.js
@@ -4,11 +4,12 @@ import {
     createDepend,
     getCandlePackage, validateEligibilityPackages
 } from "@candlelib/dev-tools";
+import { Logger } from "@candlelib/log";
 import { getPackageJsonObject } from "@candlelib/paraffin";
 import URI from "@candlelib/uri";
 import fs from "fs";
 await URI.server();
-
+const logger = Logger.get("HCToolkit").activate();
 // Grab the names of the hctoolkit packages of which we
 // will version.
 const { package: wksp_pkg, FOUND } = await getPackageJsonObject(URI.getCWDURL());
@@ -19,10 +20,15 @@ if (FOUND && wksp_pkg.name == "@hctoolkit/workspace") {
     const target_packages = await Promise.all(wksp_pkg.devPackages.map(createDepend));
 
     if (target_packages.every(d => d !== null)) {
+        try {
 
-        await validateEligibilityPackages(target_packages, (pkg) => {
-            return Object.getOwnPropertyNames(pkg?.dependencies ?? {}).filter(n => wksp_pkg.devPackages.includes(n));
-        }, false);
+            await validateEligibilityPackages(target_packages, (pkg) => {
+                return Object.getOwnPropertyNames(pkg?.dependencies ?? {}).filter(n => wksp_pkg.devPackages.includes(n));
+            }, false);
+        } catch (E) {
+            logger.log("Unable to version packages");
+            process.exit(-1);
+        }
     } else {
         throw new Error("Unable to resolve required packages");
     }
